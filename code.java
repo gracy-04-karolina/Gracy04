fc346d6be92b5bcd2a3928f0c330f0f48260196f
Gracy karolina <gracykarolina0406@gmail.com>
2025-11-15 21:21:00 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,17 @@
+public class code {
+public static void main(String[]args){
+    int num =5;
+    int i =1;
+    while(i <num){
+        i=i*2;
+    }
+    if(i == num){
+        System.out.println("power of 2");
+    }else{
+        System.out.println("not power of 2");
+    }
+    
+}
+
+    
+}
