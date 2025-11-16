73952965b23c535346304edb6105a5b79034bef5
Gracy karolina <gracykarolina0406@gmail.com>
2025-11-16 19:23:38 +0530

    Add files via upload

---
+++ studentinfo.java
@@ -0,0 +1,19 @@
+public class studentinfo {
+    public static void main(String[] args) {
+        String Name = "zara";
+        int Age = 18;
+        double Percentage = 90.5;
+        char Grade = 'A';
+        boolean ispassed = true;
+        System.out.println("Name:" + Name);
+        System.out.println("Age:" + Age);
+        System.out.println("Percentage:" + Percentage);
+        System.out.println("Grade:" + Grade);
+        if (ispassed){
+        System.out.println("passed:" + ispassed);
+        }
+        else{
+        System.out.println("not passed"); 
+        }
+    }
+}
