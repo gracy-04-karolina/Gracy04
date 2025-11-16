86e8526ff64ff806f08797ef56e9f995fcf12220
Gracy karolina <gracykarolina0406@gmail.com>
2025-11-16 19:19:42 +0530

    Add files via upload

---
+++ productofdigit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class productofdigit {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int product = 1;
+        while(n>0){
+            product = product*(n%10);
+            n = n/10;
+        }
+
+        System.out.println(product);
+    }
+    
+}
