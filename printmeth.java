9674ba179dfe767e3432e8453f313498df507baa
Gracy karolina <gracykarolina0406@gmail.com>
2025-11-16 19:20:13 +0530

    Add files via upload

---
+++ printmeth.java
@@ -0,0 +1,11 @@
+public class printmeth {
+    public static void main(String[] args) {
+        if(System.out.printf("ICE")==null){
+            System.out.print("ICE");
+        }
+        else{
+            System.out.print("CREAM");
+        }
+    }
+    
+}
